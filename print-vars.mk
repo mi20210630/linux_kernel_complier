cf69526eee7425411f6f826543d5f5f24020edcc
ShanghaiZhouWanLi <106662135+mi20210630@users.noreply.github.com>
2025-08-27 09:20:49 +0800

    Delete print-vars.mk

--- print-vars.mk
+++
@@ -1,7 +0,0 @@
-print-vars:
-	@echo "VERSION=$(shell cat Makefile | grep '^VERSION =' | cut -d' ' -f3)"
-	@echo "PATCHLEVEL=$(shell cat Makefile | grep '^PATCHLEVEL =' | cut -d' ' -f3)"
-	@echo "SUBLEVEL=$(shell cat Makefile | grep '^SUBLEVEL =' | cut -d' ' -f3)"
-	@echo "EXTRAVERSION=$(shell cat Makefile | grep '^EXTRAVERSION =' | cut -d' ' -f3-)"
-	@echo "LOCALVERSION=$(shell cat Makefile | grep '^LOCALVERSION =' | cut -d' ' -f3-)"
-	@echo "KERNELRELEASE=$(shell make -s kernelrelease)"
